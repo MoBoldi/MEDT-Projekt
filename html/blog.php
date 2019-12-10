8fb1adf5d2a116cee2b937386378b04b8a671355
Moritz Weibold <moritz@weibold.cc>
2019-12-10 09:02:58 +0100

    Irgendwie is des komisch

---
+++ html/blog.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap" rel="stylesheet">
+    <link href="../style/general.css" rel="stylesheet" type="text/css">
+    <link href="../style/blog.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <?php include "./header.html" ?>
+    <main>
+    <div class="placeholder" style="height: 100px"></div>
+    <section>
+        <div class="authorCol">
+            <img src="../data/profile.png" alt="profile picture">
+            <p class="authorName">by Max Mustermann</p>
+        </div>
+        <div class="imgCol">
+            <img src="../data/kreuzfahrt.jpg" alt="Kreuzfahrt">
+        </div>
+        <div class="textCol">
+            <h2>11 Dinge, die jeder über Kreuzfahrten wissen soll</h2> <!-- max. 49 Zeichen -->
+            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the ...</p> <!-- max.: 400 Zeichen -->
+        </div>
+    </section>
+    </main>
+    <?php include "./footer.html" ?>
+</body>
+</html>
