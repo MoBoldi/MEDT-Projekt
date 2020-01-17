c933c3563b28899559050678e65f84d709f7926a
Moritz Weibold <moritz@weibold.cc>
2020-01-17 21:07:14 +0100

    New Files - bei register, login und about fehlt das Design noch

---
+++ html/about.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>er - earth resources | home site</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap" rel="stylesheet">
+    <link href="../style/general.css" rel="stylesheet" type="text/css">
+    <link href="../style/index.css" rel="stylesheet" type="text/css">
+</head>
+
+<!--<body scroll="no" style="overflow-y: hidden" id="0">-->
+<body>
+    <?php include "./header.html" ?>
+    <h1>earth resources</h1>
+    <main>
+        <section id="1">
+            <div class="imgCol">
+                <img src="../data/statistics.jpg" alt="Statistiken Bild">
+            </div>
+            <div class="textCol">
+                <h2><a href="resources.php">resources</a></h2>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to </p>
+            </div>
+        </section>
+        <section id="2">
+            <div class="textCol left">
+                <h2><a href="protests.php">protests</a></h2>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to </p>
+            </div>
+            <div class="imgCol">
+                <img src="../data/climate01.jpg" alt="Climate Protests">
+            </div>
+        </section>
+        <section id="3">
+            <div class="imgCol">
+                <img src="../data/about01.jpg" alt="about">
+            </div>
+            <div class="textCol">
+                <h2><a href="about.php">about us</a></h2>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to </p>
+            </div>
+        </section>
+    </main>
+    <?php include "./footer.html" ?>
+    <!--<script>
+        document.getElementsByTagName('section')[0].style.paddingTop = "80px";
+        let maxBookmarks = 4;
+    </script>
+    <script src="../js/scroll.js"></script>-->
+</body>
+
+</html>
