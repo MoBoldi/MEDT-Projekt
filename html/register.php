366994291e8e5cd16c2c886aa23b3c9354ad3cca
Moritz Weibold <moritz@weibold.cc>
2020-01-20 13:07:18 +0100

    als nächstes Earth und co. fertigstellen

---
+++ html/register.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>er - earth resources | home site</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap" rel="stylesheet">
+    <link href="../style/general.css" rel="stylesheet" type="text/css">
+    <link href="../style/register.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <?php include "./header.html" ?>
+    <h1>Login</h1>
+    <div id="loginScreen">
+        <form>
+            <p>Benutzername</p>
+            <input type="text">
+            <p>Email-Adresse</p>
+            <input type="text">
+            <p>Passwort</p>
+            <input type="password">
+            <p>Passwort wiederholen</p>
+            <input type="password">
+            <br>
+            <input type="submit" value="Absenden" id="submit">
+        </form>
+    </div>
+    
+    <?php include "./footer.html" ?> 
+</body>
+
+</html>
