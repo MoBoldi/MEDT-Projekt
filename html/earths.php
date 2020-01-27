9261d406525ca48cb9af1f7de03ff5abe6510964
Moritz Weibold <moritz@weibold.cc>
2020-01-27 17:15:34 +0100

    Neuer Hintergrund; stats, numbers, earths erweitert

---
+++ html/earths.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>er - earth resources | home site</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap" rel="stylesheet">
+    <link href="../style/general.css" rel="stylesheet" type="text/css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://unpkg.com/zdog@1/dist/zdog.dist.min.js"></script>
+    <link href="../style/earths.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <?php include "./header.html" ?>
+    <h1>Ökologischer Fußabdruck</h1>
+    <main>
+        <p>Würden alle Menschen heute so leben wie wir in Europa, bräuchten wir 3 Planeten von der Qualität der Erde. Würden alle gar so leben wollen wie die US-Ameri­ka­ner­Innen, dann bräuchten wir 5 Planeten!</p>
+        <p>Da allgemein bekannt sein dürfte, dass uns nur ein einziger Planet, die Erde, zur Verfügung steht, und diese sicher nicht wachsen wird, ergibt sich daraus die größte Ungerechtigkeit in der Geschichte der Menschheit. Wir EuropäerInnen nehmen Menschen aus ärmeren Ländern Platz weg, stehen ihnen regelrecht auf den Füssen! </p>
+        <div id="illos">
+            <div class="container">
+                <canvas class="illo"></canvas>
+            </div>
+            <div class="container">
+                <canvas class="illo"></canvas>
+            </div>
+            <div class="container">
+                <canvas class="illo"></canvas>
+            </div>
+        </div>
+    </main>
+    
+    <?php include "./footer.html" ?> 
+    <script src="../js/earth.js"></script>
+
+</body>
+
+</html>
--- html/stats.php
+++ html/stats.php
@@ -6,8 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>er - earth resources | home site</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap" rel="stylesheet">
     <link href="../style/general.css" rel="stylesheet" type="text/css">
+    <link href="../style/stats.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
@@ -15,10 +17,59 @@
     <h1>Statistik</h1>
 
     <main>
-        
+        <div class="stats">
+            <h1 class="caption">
+                oil
+            </h1>
+            <div class="bar">
+                <div class="filled" id="oil"></div>
+            </div>
+        </div>
+        <div class="stats">
+            <h1 class="caption">
+                population
+            </h1>
+            <div class="bar">
+                <div class="filled" id="population"></div>
+            </div>
+        </div>
+        <div class="stats">
+            <h1 class="caption">
+                wasted food
+            </h1>
+            <div class="bar">
+                <div class="filled" id="wastedFood"></div>
+            </div>
+        </div>
     </main>
-    
-    <?php include "./footer.html" ?> 
+
+    <?php include "./footer.html" ?>
+
+    <script>
+        let oilValue = "40%";
+        let populationValue = "60%";
+        let wastedFoodValue = "33%";
+        let values = ['oil', 'population', 'wastedFood'];
+        document.getElementById('oil').style.width = '80%';
+        document.getElementById('population').style.width = '60%';
+        document.getElementById('wastedFood').style.width = '33%';
+        //document.getElementById('resourceContent').style.right = "00vw";
+
+        let chartsButton = document.getElementById("charts");
+        let earthButton = document.getElementById("earth");
+        let numbersButton = document.getElementById("numbers");
+        let resourceContent = document.getElementById("resourceContent");
+        let buttons = document.getElementsByClassName('test');
+        console.log(buttons);
+        for (let i = 0; i < buttons.length; i++) {
+            console.log('test');
+            buttons[i].addEventListener('click', animate);
+        }
+
+        function animate() {
+            console.log('test');
+        }
+    </script>
 </body>
 
 </html>
