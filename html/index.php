534ec8f6a8f2e35856146407079aaec13cbfac12
Moritz Weibold <moritz@weibold.cc>
2020-01-30 20:04:47 +0100

    numbers.php fertig; als nächstes: account.php und cms anfangen

--- html/index.php
+++ html/index.php
@@ -40,7 +40,7 @@
                 <img src="../data/about01.jpg" alt="about">
             </div>
             <div class="textCol">
-                <h2><a href="about.php">about us</a></h2>
+                <h2><a href="blog.php">visit our blog</a></h2>
                 <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to </p>
             </div>
         </section>
